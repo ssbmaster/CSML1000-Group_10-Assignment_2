77ef149eff8583e811296286be28536e99b426aa
Patrick M Osborne <posborne@yorku.ca>
2020-02-23 15:29:00 -0500

    Working ARM (apriori) model with UCI data set

--- ARM-UCI_Online_Retail_Data_Set-Patrick/ARM-UCI_Online_Retail_Data_Set.R
+++ ARM-UCI_Online_Retail_Data_Set-Patrick/ARM-UCI_Online_Retail_Data_Set.R
@@ -2,19 +2,31 @@
 library(arules)
 
 # Reading the data file
-mydata<-read.csv("./data/groceriesNOMILK.csv",header=T)
+mydata<-read.csv("./data/OnlineRetail.csv")
 
 #remove 1st column (counting # of items in basket)
-mydata <- mydata[,-1]
+#mydata <- mydata[,-1]
 
 #write updated CSV with removed column
-mydata <- write.csv(mydata, "./data/groceriesClean.csv")
+#mydata <- write.csv(mydata, "./data/groceriesClean.csv")
 
 #read updated CSV into transactions object
-trans <- read.transactions("./data/groceriesClean.csv", format="basket", sep=",", cols=1)
+#trans <- read.transactions("./data/groceriesClean.csv", format="single", sep=",", cols=1)
+
+colvec <- c("InvoiceNo","Description")
+
+# read transactions from csv
+trans <- read.transactions(
+  file = "./data/OnlineRetail.csv",
+  format = "single",
+  header = TRUE,
+  sep = ",",
+  cols=colvec,
+  rm.duplicates = T
+)
 
 itemFrequencyPlot(trans,topN=20,type="relative",main="Relative Item Frequency Plot")
 
-association.rules <- apriori(trans, parameter = list(supp=0.001, conf=0.8,maxlen=3))
+association.rules <- apriori(trans, parameter = list(supp=0.01, conf=0.7,maxlen=15))
 
 inspect(association.rules)
