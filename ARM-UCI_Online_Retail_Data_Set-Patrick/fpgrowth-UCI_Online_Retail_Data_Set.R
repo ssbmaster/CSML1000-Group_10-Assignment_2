f95d1b3d7f2d8556dfc4512a8ec29ba858c2e74b
alexf388 <alexchunkwan.fung@gmail.com>
2020-02-29 04:18:53 -0500

    FPGrowth works with Sparkylr (fp_growth_sparkylr.r). Also generated a HTML network of the FPGrowth with Sparkly (fpgrowth_sparklyr_network.html)

--- ARM-UCI_Online_Retail_Data_Set-Patrick/fpgrowth-UCI_Online_Retail_Data_Set.R
+++ ARM-UCI_Online_Retail_Data_Set-Patrick/fpgrowth-UCI_Online_Retail_Data_Set.R
@@ -14,6 +14,18 @@ library(arules)
 
 data<-read.csv("./data/online_retail_II_combined_clean.csv")
 
+# colvec <- c("X","InvoiceNo","Description")
+# 
+# trans <- arules::read.transactions(
+#   file = "./data/online_retail_II_combined_clean.csv",
+#   format = "basket",
+#   header = TRUE,
+#   sep = ",",
+#   cols=colvec,
+#   rm.duplicates = T
+# )
+
+
 colvec <- c("InvoiceNo","Description")
 
 trans <- arules::read.transactions(
@@ -24,19 +36,31 @@ trans <- arules::read.transactions(
   cols=colvec,
   rm.duplicates = T
 )
+
 memory.limit(size = 56000)
 data<-read.csv("./data/online_retail_II_combined_clean.csv")
-data <- data[ -c(2, 4:8) ]
-train <- sapply(data, as.factor)
+data_cols_removed <- data[ -c(1, 2, 4, 6:10) ]
+train <- sapply(data_cols_removed, as.factor)
 train <- data.frame(train, check.names=FALSE)
+
+#remove duplicates from train
+train_no_duplicates = train[!duplicated(train), ]
+
+#including this because allocation vector size is WAY TOO BIG
+#samp <- sample(nrow(train),10000) #500000 rows
+#txns <- as(train[samp,],"transactions")
 txns <- as(train,"transactions")
 
+#fpgroowth with trans
+# association.rules = rCBA::fpgrowth(trans, support = 0.005, confidence = 0.5, maxLength = 20,
+#          consequent = "Description", verbose = TRUE, parallel = FALSE)
 
+#fpgroowth with txns
 association.rules = rCBA::fpgrowth(txns, support = 0.005, confidence = 0.5, maxLength = 20,
-         consequent = "Description", verbose = TRUE, parallel = FALSE)
+          consequent = "Description", verbose = TRUE, parallel = FALSE)
 
 
-predictions <- rCBA::classification(train,rules)
+Tickepredictions <- rCBA::classification(train,rules)
 table(predictions)
 sum(as.character(train$Species)==as.character(predictions),na.rm=TRUE)/length(predictions)
 
